facb2f6e9b4e079d9a1c4342a415584edcc18a74
Fillipe de Souza Silva <fssilva@fiecalunos.lan>
2026-05-14 20:43:20 -0300

    Quarto commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn Github Action na Fiec
         </a>
       </header>
     </div>
